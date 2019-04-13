985e50f0f4cad6e883b482cd2dbf9bb4fd59f2d1
Tyra M <t.nicole.education@gmail.com>
2019-04-13 12:58:42 -0600

    app-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      colorList: ["blue", "green", "yellow", "purple", "red"]
+    };
+  }
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+    let colorFlash = this.state.colorList.map((element, index)=>{
+      return <h3 key={index}>{element}</h3>
+    })
+    return <div>
+          <h1 className="h1">This is App #2</h1>;
+
+<h2 className="h2">These are my favorite colors!</h2>
+    
+    <div className="App">{colorFlash}</div>
+    </div>
   }
 }
 
